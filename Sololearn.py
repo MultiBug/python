6aaa8961c5a97ff16810bdfbd9026ab0300d2e07
vladislav <multibug90@gmail.com>
2021-04-02 09:41:55 +0300

    sololearn

---
+++ Sololearn.py
@@ -0,0 +1,51 @@
+print("www.sololearn.com")
+print('\n')
+"""
+module 1
+"""
+print('========== Module 1: Basics ==========')
+print('Hello world!')
+print('--------------------')
+print('Многострочный \nвывод строки \nс переносом')
+print('--------------------')
+print('float 3/4 = ' + str(3 / 4))
+print('float ' + str(0.42))
+print('Возведение в степень 2^5 = ' + str(2 ** 5))
+print('Возведение в степень 9^0.5 = ' + str(9 ** (1 / 2)))
+print('Целочисленное деление 20//6 = ' + str(20 // 6))
+print('Целочисленное деление 20//6 = ' + str(20 // 6))
+print('Деление по модулю 20%6 = ' + str(20 % 6))
+print('Деление по модулю (float) 1.25%0.5 = ' + str(1.25 % 0.5))
+print('\n')
+"""
+module 2
+"""
+print('========== Module 2: Strings & Variables ==========')
+print('Вывод в разных ковычках:')
+print("\"Python is fun!\"")
+print('\'Python is fun!\'')
+print('Экранирование:')
+print('\'Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!\'')
+print("\"Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!\"")
+print('\n')
+print("""this 
+is a 
+multiline text""")
+print("Spam" + ' eggs')
+print("2" + " 2")
+print("spam " * 3)
+print(4*'2 ')
+print('user = \"James\"')
+print('this_is_a_normal_name = 7')
+print('123abc = 7 SyntaxError: invalid syntax')
+x = 7
+print(x)
+print(x+3)
+x = 123.456
+print(x)
+x = "This is a string"
+print(x+'!')
+x = input()
+print(x)
+name = input("Enter your name: ")
+print("Hello, "+name)
