11b32c70b72ab8ba8f005ee3d4273c63f7378ea1
vladislav <multibug90@gmail.com>
2021-04-02 08:57:40 +0300

    sololearn

---
+++ main.py
@@ -0,0 +1,30 @@
+print("www.sololearn.com")
+print('\n')
+"""
+module 1
+"""
+print('========== Module 1: Basics ==========')
+print('Hello world!')
+print('--------------------')
+print('Многострочный \nвывод строки \nс переносом')
+print('--------------------')
+print('float 3/4 = ' + str(3 / 4))
+print('float ' + str(0.42))
+print('Возведение в степень 2^5 = ' + str(2 ** 5))
+print('Возведение в степень 9^0.5 = ' + str(9 ** (1 / 2)))
+print('Целочисленное деление 20//6 = ' + str(20 // 6))
+print('Целочисленное деление 20//6 = ' + str(20 // 6))
+print('Деление по модулю 20%6 = ' + str(20 % 6))
+print('Деление по модулю (float) 1.25%0.5 = ' + str(1.25 % 0.5))
+print('\n')
+"""
+module 2
+"""
+print('========== Module 2: Strings & Variables ==========')
+print('Вывод в разных ковычках:')
+print("\"Python is fun!\"")
+print('\'Python is fun!\'')
+print('Экранирование:')
+print('\'Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!\'')
+print("\"Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!\"")
+print('\n')
