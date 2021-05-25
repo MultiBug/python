7786fb731f100c16b84dda210f9e3e21becdf5e2
MultiBug <multibug90@gmail.com>
2021-05-25 13:45:31 +0300

    Lesson 9

---
+++ 2_strings_and_variables/Lesson_9/Lesson_9.py
@@ -0,0 +1,10 @@
+####################################
+# 9.1 Строки
+print("Python is fun!")
+print('Always look on the bright side of life')
+####################################
+
+####################################
+# 9.2 Backslash
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+####################################
