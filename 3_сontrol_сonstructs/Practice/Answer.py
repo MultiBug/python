e108d3d0abf2f29f1e500be07a3a04b1aa7e27a1
MultiBug <multibug90@gmail.com>
2021-05-26 14:28:50 +0300

    Tasks and Answer (Lesson 20)

--- 3_сontrol_сonstructs/Practice/Answer.py
+++ 3_сontrol_сonstructs/Practice/Answer.py
@@ -1,4 +1,10 @@
 ########################
-# Решение задачи 0.0 '':
-
+# Решение задачи 20.2 'Мороженое для всех!':
+money = int(input())
+price = int(input())
+x = price*10
+if money <= money + price:
+    if money >= x:
+        y = money % x
+    print(y)
 ########################
