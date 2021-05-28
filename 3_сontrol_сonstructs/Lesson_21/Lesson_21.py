056b62a56a3ce1018bce32ff63bc9c6e311d5eb2
MultiBug <multibug90@gmail.com>
2021-05-28 14:35:52 +0300

    Lesson 21

---
+++ 3_сontrol_сonstructs/Lesson_21/Lesson_21.py
@@ -0,0 +1,34 @@
+####################################
+# 21.1 Инструкции else
+x = 4
+if x == 5:
+    print("Yes")
+else:
+    print("No")
+####################################
+####################################
+# 21.2 Инструкции else
+num = 3
+if num == 1:
+  print("One")
+else:
+  if num == 2:
+    print("Two")
+  else:
+    if num == 3:
+      print("Three")
+    else:
+      print("Something else")
+####################################
+####################################
+# 21.3 Инструкции elif
+num = 3
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Three")
+else:
+    print("Something else")
+####################################
