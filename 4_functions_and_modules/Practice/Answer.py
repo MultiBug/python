6a88fd22367e6550ddaa435d93d3f0db6fd2a9a3
MultiBug <multibug90@gmail.com>
2021-06-01 15:33:37 +0300

    Tasks and Answer (Lesson 34)

--- 4_functions_and_modules/Practice/Answer.py
+++ 4_functions_and_modules/Practice/Answer.py
@@ -5,3 +5,24 @@ def welcome_message():
     print("Welcome,", name)
 welcome_message()
 ########################
+########################
+# Решение задачи 34.2 'Совпадение паролей':
+password = input()
+repeat = input()
+def validate(password, repeat):
+    if password == repeat:
+        print('Correct')
+    else:
+        print('Wrong')
+validate(password, repeat)
+########################
+########################
+# Решение задачи 34.3 'Площадь прямоугольника':
+length = int(input())
+width = int(input())
+def area(length, width):
+    print(int(length) * int(width))
+    if length == width:
+        print('Square')
+area(length, width)
+########################
