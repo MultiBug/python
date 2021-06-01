309f12124ecc63a95dc8ba80a9d2b205d974d0ce
MultiBug <multibug90@gmail.com>
2021-06-01 12:57:56 +0300

    Tasks and Answer (Lesson 31)

--- 3_сontrol_сonstructs/Practice/Answer.py
+++ 3_сontrol_сonstructs/Practice/Answer.py
@@ -208,3 +208,18 @@ b = int(input())
 date = list(range(a, b))
 print(date)
 ########################
+########################
+# Решение задачи 31 'FizzBuzz':
+n = int(input())
+for x in range(1, n):
+    if x % 2 == 0:
+        print()
+    elif x % 3 == 0 and x % 5 == 0:
+        print("SoloLearn")
+    elif x % 3 == 0:
+        print("Solo")
+    elif x % 5 == 0:
+        print("Learn")
+    else:
+        print(x)
+########################
--- test.py
+++
@@ -1,4 +0,0 @@
-a = int(input())
-b = int(input())
-date = list(range(a, b))
-print(date)
