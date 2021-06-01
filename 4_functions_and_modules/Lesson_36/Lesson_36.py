d3199525f8138acc49fd95fe7fd13fa1f58ab0f4
MultiBug <multibug90@gmail.com>
2021-06-01 16:17:23 +0300

    Lesson 36

---
+++ 4_functions_and_modules/Lesson_36/Lesson_36.py
@@ -0,0 +1,19 @@
+####################################
+# 36.1 Комментарии
+x = 365
+y = 7
+# this is a comment
+print(x % y) # find the remainder
+# print (x // y)
+# another comment
+####################################
+####################################
+# 36.2 Строки документации
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+shout("spam")
+####################################
