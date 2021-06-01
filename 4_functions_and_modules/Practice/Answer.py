ba0b45cc4f64660b65c4c4e6efa2a2767b2f154a
MultiBug <multibug90@gmail.com>
2021-06-01 14:10:39 +0300

    Tasks and Answer (Lesson 33)

---
+++ 4_functions_and_modules/Practice/Answer.py
@@ -0,0 +1,7 @@
+########################
+# Решение задачи 33.2 'Добро пожаловать в SoloLearn, username':
+def welcome_message():
+    name = input()
+    print("Welcome,", name)
+welcome_message()
+########################
