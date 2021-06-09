40a911bc727f8fbed15c814c7cb26bd034e8ac3d
MultiBug <multibug90@gmail.com>
2021-06-09 14:15:17 +0300

    Tasks and Answer (Lesson 43)

--- 5_exceptions_and_files/Practice/Answer.py
+++ 5_exceptions_and_files/Practice/Answer.py
@@ -1,4 +1,9 @@
 ########################
 # Решение задачи 43.3 'Система ПИН-кодов банковский карт':
-
+pin = input()
+try:
+    pin = int(pin)
+    print("PIN code is created")
+except ValueError:
+    print("Please enter a number")
 ########################
