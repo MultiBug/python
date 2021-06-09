11526d72ef6cd93c68df7ca799be1293f5c25142
MultiBug <multibug90@gmail.com>
2021-06-09 15:08:59 +0300

    Lesson 46

---
+++ 5_exceptions_and_files/Lesson_46/Lesson_46.py
@@ -0,0 +1,20 @@
+####################################
+# 46.1 Открытие файлов
+myfile = open("filename.txt")
+####################################
+####################################
+# 46.2 Открытие файлов
+# write mode
+open("filename.txt", "w")
+# read mode
+open("filename.txt", "r")
+open("filename.txt")
+# binary write mode
+open("filename.txt", "wb")
+####################################
+####################################
+# 46.3 Открытие файлов
+file = open("filename.txt", "w")
+# do stuff to the file
+file.close()
+####################################
