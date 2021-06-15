73e00603a7070b01a1b46a49e951eb4395fa2a03
MultiBug <multibug90@gmail.com>
2021-06-15 17:35:28 +0300

    Tasks and Answer (Lesson 55)

--- 6_other_types_of_objects/Practice/Answer.py
+++ 6_other_types_of_objects/Practice/Answer.py
@@ -21,5 +21,14 @@ else:
 ########################
 ########################
 # Решение задачи 55.3 'Где книга?':
-
+books = {
+    "Life of Pi": "Adventure Fiction",
+    "The Three Musketeers": "Historical Adventure",
+    "Watchmen": "Comics",
+    "Bird Box": "Horror",
+    "Harry Potter":"Fantasy Fiction",
+    "Good Omens": "Comedy"
+}
+book = input()
+print(books.get(book, "Book not found"))
 ########################
